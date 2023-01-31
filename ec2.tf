5083489ccb0619d1dbafe16210f1a5b418826acf
raheemkhan77 <55932523+raheemkhan77@users.noreply.github.com>
2023-01-31 18:47:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,14 +6,14 @@ resource "aws_instance" "devec2" {
   vpc_security_group_ids = [ "sgr-09bf3d0994968a732" , "sgr-0fba24e3deb3bd92b"   ]
    count         = "2"
    tags = {
-     "Name" = "aptrac-server ${count.index}"
+     "Name" = "dev-server ${count.index}"
    }
   
 }
 /* commenting key pair block 
 # create a key pair
-resource "aws_key_pair" "aptrac.ec2keypair" {
-  key_name = "aptrac-ec2key"
+resource "aws_key_pair" "dev.ec2keypair" {
+  key_name = "dev-ec2key"
   public_key = "ssh-rsa
   some key value to be generated using ssh- keygen and copy the whole key here" 
   
